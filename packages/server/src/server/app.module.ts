9418661515ccd1964ee3356ba013415302b0b181
Dave Thomas <dave@monstergfx.com>
2022-09-26 14:12:50 -0700

    chore: add portainer service files

--- packages/server/src/server/app.module.ts
+++ packages/server/src/server/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RenderModule } from 'nest-next';
+import { PortainerService } from './services/portainer.service';
 import Next from 'next';
 
 @Module({
@@ -9,6 +10,6 @@ import Next from 'next';
     RenderModule.forRootAsync(Next({ dev: true }), { viewsDir: null }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PortainerService],
 })
 export class AppModule {}
