1190b880c2587acc00cd1b4264051e7817426332
Dave Thomas <dave@monstergfx.com>
2022-09-26 14:54:16 -0700

    chore: add symbols for injection

--- packages/server/src/server/app.types.ts
+++ packages/server/src/server/app.types.ts
@@ -2,4 +2,8 @@ export enum EnvironmentVariables {
   PORTAINER_HOST = 'PORTAINER_HOST',
   PORTAINER_USER = 'PORTAINER_USER',
   PORTAINER_PASSWORD = 'PORTAINER_PASSWORD',
-};
+}
+
+export const Symbols = Object.freeze({
+  Axios: Symbol('Axios'),
+});
