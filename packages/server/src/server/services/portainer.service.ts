d031d7355f26224f1b430742aabc58f9c2c14823
Dave Thomas <dave@monstergfx.com>
2022-09-28 17:24:09 -0700

    chore: implement listMinecraftContainers method

--- packages/server/src/server/services/portainer.service.ts
+++ packages/server/src/server/services/portainer.service.ts
@@ -5,6 +5,13 @@ import axios from 'axios';
 
 @Injectable()
 export class PortainerService {
+  // @TODO make this private
+  public token: string | undefined = undefined;
+
+  private baseUrl: string;
+  private username: string;
+  private password: string;
+
   constructor(
     @Optional() @Inject(Symbols.Axios) private readonly axiosLib: typeof axios,
     private readonly configService: ConfigService,
@@ -12,6 +19,16 @@ export class PortainerService {
     if (!this.axiosLib) {
       this.axiosLib = axios;
     }
+
+    this.baseUrl = this.configService.get<string>(
+      EnvironmentVariables.PORTAINER_URL,
+    );
+    this.username = this.configService.get<string>(
+      EnvironmentVariables.PORTAINER_USER,
+    );
+    this.password = this.configService.get<string>(
+      EnvironmentVariables.PORTAINER_PASSWORD,
+    );
   }
 
   /*
@@ -29,17 +46,49 @@ export class PortainerService {
 
   */
 
-  public async getAuthToken(
-    host: string,
-    username: string,
-    password: string,
-  ): Promise<string> {
-    const url = new URL('/api/auth', host);
+  public async getAuthToken(): Promise<void> {
+    const url = new URL('/api/auth', this.baseUrl);
     const response = await this.axiosLib({
       method: 'post',
       url: url.toString(),
-      data: { username, password },
+      data: { username: this.username, password: this.password },
     });
-    return response?.data?.token;
+    this.token = response?.data?.token;
+  }
+
+  public async listMinecraftStacks(): Promise<any[]> {
+    if (!this.token) {
+      await this.getAuthToken();
+    }
+    const url = new URL('/api/stacks', this.baseUrl);
+    let count = 0;
+    let status = 0;
+    let data: any;
+    while (status !== 200) {
+      count++;
+      if (count > 4) {
+        throw new Error('unable to authenticate against portainer api');
+      }
+      const response = await this.axiosLib({
+        method: 'get',
+        url: url.toString(),
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      });
+      status = response.status;
+      data = response.data;
+      if (status === 401) {
+        await this.getAuthToken();
+      }
+    }
+    // @TODO get additional information for the stacks
+    return (data || [])
+      .filter(({ Name }) => /minecraft/i.test(Name))
+      .map((stack) => ({
+        id: stack.Id,
+        name: stack.Name,
+        status: stack.Status,
+      }));
   }
 }
