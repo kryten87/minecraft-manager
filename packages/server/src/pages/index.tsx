9ca93f878bd3bf11590d952dcb9c2528b9b37529
Dave Thomas <dave@monstergfx.com>
2022-09-28 21:26:16 -0700

    chore: add stack list to index component

--- packages/server/src/pages/index.tsx
+++ packages/server/src/pages/index.tsx
@@ -1,8 +1,20 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import '@picocss/pico/css/pico.css';
+import { GetServerSideProps } from 'next';
 
-const Home: FC = () => {
-    return <h1>Home</h1>;
+const Home: FC = (props: Record<string, any>) => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <pre>{ JSON.stringify(props.containers, null, 2) }</pre>
+    </div>
+  );
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps<any> = async (ctx) => {
+  return {
+    props: { containers: ctx?.query?.containers || [] }
+  };
+};
