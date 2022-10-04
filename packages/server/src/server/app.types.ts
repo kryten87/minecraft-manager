994c00ef3db1e74c54ef6db4f90e84385260b113
Dave Thomas <dave@monstergfx.com>
2022-10-04 08:53:31 -0700

    chore: add default server config

--- packages/server/src/server/app.types.ts
+++ packages/server/src/server/app.types.ts
@@ -30,9 +30,9 @@ export enum MinecraftLevelType {
 
 export interface MinecraftStackConfig {
   motd: string; // message of the day
-  whitelist: string; // CSV list of allowed players
-  ops: string; // CSV list of admins
-  icon: string; // URL to icon file
+  whitelist: string; // CSV list of allowed players (Kryten,MWThomas,EMThomas)
+  ops: string; // CSV list of admins (Kryten,MWThomas,EMThomas)
+  icon: string; // URL to icon file (http://findicons.com/files/icons/2438/minecraft/256/minecraft.png)
   allowNether: boolean; // a flag to allow/disallow nether (true)
   announcePlayerAchievements: boolean; // a flag to allow/disallow announcements (true)
   enableCommandBlock: boolean; // a flag to allow/disallow command blocks (true)
@@ -45,7 +45,7 @@ export interface MinecraftStackConfig {
   spawnMonsters: boolean; // (true)
   spawnNPCs: boolean; // spawn villagers (true)
   spawnProtection: number; // set the area that non-ops can not edit (0 to disable) @TODO research this
-  viewDistance: number; // set the amount of world data the server sends, in chunks (???)
+  viewDistance: number; // set the amount of world data the server sends, in chunks (10)
   seed: string; // the level seed value (undefined)
   gameMode: MinecraftGameMode; // (survival)
   pvp: boolean; // (false)
@@ -56,3 +56,28 @@ export interface MinecraftStackConfig {
   allowFlight: boolean; // allow flight, if the mode allows (true)
   serverName: string; // server name for online finding (unefined)
 }
+
+export const defaultMinecraftConfig: Partial<MinecraftStackConfig> =
+  Object.freeze({
+    whitelist: 'Kryten,MWThomas,EMThomas',
+    ops: 'Kryten,MWThomas,EMThomas',
+    icon: 'http://findicons.com/files/icons/2438/minecraft/256/minecraft.png',
+    allowNether: true,
+    announcePlayerAchievements: true,
+    enableCommandBlock: true,
+    generateStructures: true,
+    hardcode: false,
+    snooperEnabled: false,
+    maxBuildHeight: 256,
+    maxTickTime: 60000,
+    spawnAnimals: true,
+    spawnMonsters: true,
+    spawnNPCs: true,
+    spawnProtection: 0,
+    viewDistance: 10,
+    gameMode: MinecraftGameMode.survival,
+    pvp: false,
+    levelType: MinecraftLevelType.normal,
+    onlineMode: true,
+    allowFlight: true,
+  });
