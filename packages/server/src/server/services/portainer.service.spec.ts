a954542541d755ab178f4ac681cabe601b5f6ab2
Dave Thomas <dave@monstergfx.com>
2022-10-11 08:08:20 -0700

    test: add tests for createStackName

--- packages/server/src/server/services/portainer.service.spec.ts
+++ packages/server/src/server/services/portainer.service.spec.ts
@@ -56,6 +56,26 @@ describe('PortainerService', () => {
     expect(service).toBeDefined();
   });
 
+  describe('createStackName', () => {
+    it('should return the expected value', () => {
+      [
+        { name: 'hello', expected: 'hello' },
+        { name: 'hello world', expected: 'hello_world' },
+        { name: 'hello  world       today', expected: 'hello_world_today' },
+        { name: 'hello#world', expected: 'helloworld' },
+        { name: '42-hello', expected: '42-hello' },
+        { name: 'someThing', expected: 'someThing' },
+        {
+          name: 'nice `~!@#$%^&*()+=[]{}|;:\'",.<>/? world',
+          expected: 'nice_world',
+        },
+      ].forEach(({ name, expected }) => {
+        const res = service.createStackName(name);
+        expect(res).toBe(expected);
+      });
+    });
+  });
+
   describe('getAuthToken', () => {
     describe('state = no token', () => {
       beforeEach(() => {
