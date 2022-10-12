08c3d2fdba8165a8ab364143af9b8d6597771629
Dave Thomas <dave@monstergfx.com>
2022-10-11 17:03:00 -0700

    chore: implement stack list

--- packages/server/src/pages/index.tsx
+++ packages/server/src/pages/index.tsx
@@ -1,12 +1,28 @@
 import { FC, useEffect, useState } from 'react';
 import '@picocss/pico/css/pico.css';
 import { GetServerSideProps } from 'next';
+import { PortainerStatus, StackListAction } from '../shared/types';
+import axios from 'axios';
+import { StackList } from '../client/components/StackList';
+
+const pause = (duration: number) => new Promise((resolve) => setTimeout(resolve, duration));
 
 const Home: FC = (props: Record<string, any>) => {
+  const [stacks, setStacks] = useState(props?.containers || []);
+  const [isShowingList, setIsShowingList] = useState(true);
+  const [isProcessing, setIsProcessing] = useState(false);
+
+  const onStackRefresh = (stacks: any[]) => {
+    setStacks(stacks);
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
-      <pre>{ JSON.stringify(props.containers, null, 2) }</pre>
+    <div className="container">
+      <h1>Minecraft Servers</h1>
+      { isShowingList
+        ? (<StackList stacks={ stacks } onStackRefresh={ onStackRefresh } />)
+        : (<div>not implemented</div>)
+      }
     </div>
   );
 };
