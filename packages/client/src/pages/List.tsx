29d50191576a45c74d94f1198a774f81761ccc81
Dave Thomas <davet@ifit.com>
2022-10-13 22:34:17 -0700

    chore: add list page component

--- packages/client/src/pages/List.tsx
+++ packages/client/src/pages/List.tsx
@@ -1,5 +1,20 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import '@picocss/pico/css/pico.css';
+import { StackList } from '../components/StackList';
+import { Link } from 'react-router-dom';
 
 export const List: FC = (props: Record<string, any>) => {
-  return <h1>List Page</h1>;
+  const [stacks, setStacks] = useState(props?.containers || []);
+
+  // @TODO on load, refresh stacks
+
+  const onStackRefresh = (updatedStacks: any[]) => {
+    setStacks(updatedStacks);
+  };
+
+  return (
+    <div className="container" style={{ marginTop: '2em' }}>
+      <StackList stacks={ stacks } onStackRefresh={ onStackRefresh } />
+    </div>
+  );
 };
