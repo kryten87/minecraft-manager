ec560580a6db026289d0359469da166af7c9a535
Dave Thomas <davet@ifit.com>
2022-10-14 13:36:39 -0700

    chore: remove references to next js

--- packages/server/src/server/app.module.ts
+++ packages/server/src/server/app.module.ts
@@ -1,12 +1,10 @@
 import { ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { PortainerService } from './services/portainer.service';
-import { RenderModule } from 'nest-next';
 import { ApiController } from './api.controller';
-import Next from 'next';
 
 @Module({
-  imports: [RenderModule.forRootAsync(Next({ dev: true }), { viewsDir: null })],
+  imports: [],
   controllers: [ApiController],
   providers: [ConfigService, PortainerService],
 })
