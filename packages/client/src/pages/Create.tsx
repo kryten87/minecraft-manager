323a954691aa2eb8124fcb961261a060e29ac2e3
Dave Thomas <davet@ifit.com>
2022-10-13 21:48:08 -0700

    chore: add router

---
+++ packages/client/src/pages/Create.tsx
@@ -0,0 +1,5 @@
+import { FC } from 'react';
+
+export const Create: FC = (props: Record<string, any>) => {
+  return <h1>Create Page</h1>;
+};
---
+++ packages/client/src/pages/List.tsx
@@ -0,0 +1,5 @@
+import { FC } from 'react';
+
+export const List: FC = (props: Record<string, any>) => {
+  return <h1>List Page</h1>;
+};
