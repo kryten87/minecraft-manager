4a7124cd660d71a30c739bc70d417661c67c3818
Dave Thomas <davet@ifit.com>
2022-10-13 22:35:13 -0700

    chore: remove unnecessary markup

--- packages/client/src/pages/Create.tsx
+++ packages/client/src/pages/Create.tsx
@@ -71,122 +71,120 @@ export const Create: FC = (props: Record<string, any>) => {
   };
 
   return (
-    <>
-      <form>
-        <TextInput
-          name="name"
-          label="Name"
-          placeholder="Server name"
-          value={ metadata.name || '' }
-          required
-          onChange={ onChangeMetadata }
-          error={ errors.name }
-        />
+    <form>
+      <TextInput
+        name="name"
+        label="Name"
+        placeholder="Server name"
+        value={ metadata.name || '' }
+        required
+        onChange={ onChangeMetadata }
+        error={ errors.name }
+      />
 
-        <label htmlFor="description">
-          Description
-          <textarea id="description" name="description" placeholder="Description" required onChange={ (event) => onChangeMetadata('description', event.currentTarget.value) } />
-        </label>
+      <label htmlFor="description">
+        Description
+        <textarea id="description" name="description" placeholder="Description" required onChange={ (event) => onChangeMetadata('description', event.currentTarget.value) } />
+      </label>
 
 
-        <Select
-          name="owner"
-          label="Owner"
-          required
-          nullMessage="Select an owner..."
-          value={ metadata.owner || '' }
-          error={ errors.owner }
-          options={ [
-            { label: 'Evan', value: 'Evan' },
-            { label: 'Maxwell', value: 'Maxwell' },
-            { label: 'Daddy', value: 'Daddy' },
-          ] }
-          onChange={ onChangeMetadata }
-        />
+      <Select
+        name="owner"
+        label="Owner"
+        required
+        nullMessage="Select an owner..."
+        value={ metadata.owner || '' }
+        error={ errors.owner }
+        options={ [
+          { label: 'Evan', value: 'Evan' },
+          { label: 'Maxwell', value: 'Maxwell' },
+          { label: 'Daddy', value: 'Daddy' },
+        ] }
+        onChange={ onChangeMetadata }
+      />
 
-        <TextInput
-          name="seed"
-          label="Seed"
-          placeholder="Seed value"
-          value={ config.seed || '' }
-          onChange={ onChangeConfig }
-        />
+      <TextInput
+        name="seed"
+        label="Seed"
+        placeholder="Seed value"
+        value={ config.seed || '' }
+        onChange={ onChangeConfig }
+      />
 
-        <div className="grid">
-          {/* column 1 */}
-          <div>
-            <Select
-              name="gameMode"
-              label="Game Mode"
-              required
-              nullMessage="Select a game mode..."
-              value={ `${config.gameMode}` }
-              error={ errors.gameMode }
-              options={ [
-                { label: 'Creative', value: 'creative' },
-                { label: 'Survival', value: 'survival' },
-                { label: 'Adventure', value: 'adventure' },
-                { label: 'Spectator', value: 'spectator' },
-              ] }
-              onChange={ onChangeConfig }
-            />
+      <div className="grid">
+        {/* column 1 */}
+        <div>
+          <Select
+            name="gameMode"
+            label="Game Mode"
+            required
+            nullMessage="Select a game mode..."
+            value={ `${config.gameMode}` }
+            error={ errors.gameMode }
+            options={ [
+              { label: 'Creative', value: 'creative' },
+              { label: 'Survival', value: 'survival' },
+              { label: 'Adventure', value: 'adventure' },
+              { label: 'Spectator', value: 'spectator' },
+            ] }
+            onChange={ onChangeConfig }
+          />
 
-            <Select
-              name="levelType"
-              label="Level Type"
-              required
-              nullMessage="Select a level type..."
-              value={ `${config.levelType}` }
-              error={ errors.levelType }
-              options={ [
-                { label: 'Normal', value: 'minecraft:normal' },
-                { label: 'Flat', value: 'minecraft:flat' },
-                { label: 'Large Biomes', value: 'minecraft:large_biomes' },
-                { label: 'Amplified', value: 'minecraft:amplified' },
-                { label: 'Single Biome', value: 'minecraft:single_biome_surface' },
-              ] }
-              onChange={ onChangeConfig }
-            />
+          <Select
+            name="levelType"
+            label="Level Type"
+            required
+            nullMessage="Select a level type..."
+            value={ `${config.levelType}` }
+            error={ errors.levelType }
+            options={ [
+              { label: 'Normal', value: 'minecraft:normal' },
+              { label: 'Flat', value: 'minecraft:flat' },
+              { label: 'Large Biomes', value: 'minecraft:large_biomes' },
+              { label: 'Amplified', value: 'minecraft:amplified' },
+              { label: 'Single Biome', value: 'minecraft:single_biome_surface' },
+            ] }
+            onChange={ onChangeConfig }
+          />
 
-            <Select
-              name="difficulty"
-              label="Difficulty"
-              required
-              nullMessage="Select a difficulty..."
-              value={ `${config.difficulty}` }
-              error={ errors.difficulty }
-              options={ [
-                { label: 'Peaceful', value: '0' },
-                { label: 'Easy', value: '1' },
-                { label: 'Normal', value: '2' },
-                { label: 'Hard', value: '3' },
-              ] }
-              onChange={ onChangeConfig }
-            />
+          <Select
+            name="difficulty"
+            label="Difficulty"
+            required
+            nullMessage="Select a difficulty..."
+            value={ `${config.difficulty}` }
+            error={ errors.difficulty }
+            options={ [
+              { label: 'Peaceful', value: '0' },
+              { label: 'Easy', value: '1' },
+              { label: 'Normal', value: '2' },
+              { label: 'Hard', value: '3' },
+            ] }
+            onChange={ onChangeConfig }
+          />
 
-            <Checkbox name="hardcore" label="Hardcore" value={ !!config.hardcore } onChange={ onChangeConfig } />
-          </div>
-
-          {/* column 2 */}
-          <div>
-            <Checkbox name="allowNether" label="Allow Nether" value={ !!config.allowNether } onChange={ onChangeConfig } />
-            <Checkbox name="generateStructures" label="Generate Structures" value={ !!config.generateStructures } onChange={ onChangeConfig } />
-            <Checkbox name="spawnAnimals" label="Spawn Animals" value={ !!config.spawnAnimals } onChange={ onChangeConfig } />
-            <Checkbox name="spawnMonsters" label="Spawn Monsters" value={ !!config.spawnMonsters } onChange={ onChangeConfig } />
-            <Checkbox name="spawnNpcs" label="Spawn NPCs" value={ !!config.spawnNpcs } onChange={ onChangeConfig } />
-            <Checkbox name="announcePlayerAchievements" label="Announce Player Achievements" value={ !!config.announcePlayerAchievements } onChange={ onChangeConfig } />
-            <br />
-            <Checkbox name="pvp" label="Enable PvP" value={ !!config.pvp } onChange={ onChangeConfig } />
-          </div>
+          <Checkbox name="hardcore" label="Hardcore" value={ !!config.hardcore } onChange={ onChangeConfig } />
         </div>
 
-        <br />
-        <button
-          type="button"
-          disabled={ saveIsDisabled }
-          onClick={ onClickSave }
-        >Save &amp; Start the Server!</button>
-      </form>
-    </>
+        {/* column 2 */}
+        <div>
+          <Checkbox name="allowNether" label="Allow Nether" value={ !!config.allowNether } onChange={ onChangeConfig } />
+          <Checkbox name="generateStructures" label="Generate Structures" value={ !!config.generateStructures } onChange={ onChangeConfig } />
+          <Checkbox name="spawnAnimals" label="Spawn Animals" value={ !!config.spawnAnimals } onChange={ onChangeConfig } />
+          <Checkbox name="spawnMonsters" label="Spawn Monsters" value={ !!config.spawnMonsters } onChange={ onChangeConfig } />
+          <Checkbox name="spawnNpcs" label="Spawn NPCs" value={ !!config.spawnNpcs } onChange={ onChangeConfig } />
+          <Checkbox name="announcePlayerAchievements" label="Announce Player Achievements" value={ !!config.announcePlayerAchievements } onChange={ onChangeConfig } />
+          <br />
+          <Checkbox name="pvp" label="Enable PvP" value={ !!config.pvp } onChange={ onChangeConfig } />
+        </div>
+      </div>
+
+      <br />
+      <button
+        type="button"
+        disabled={ saveIsDisabled }
+        onClick={ onClickSave }
+      >Save &amp; Start the Server!</button>
+    </form>
   );
 };
