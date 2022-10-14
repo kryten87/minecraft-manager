f3f907e77011231b0e8fac6ed23a37f0e4a6968a
Dave Thomas <davet@ifit.com>
2022-10-14 13:46:29 -0700

    chore: correct config module init

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,10 +1,10 @@
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { PortainerService } from './services/portainer.service';
 import { ApiController } from './api.controller';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [ApiController],
   providers: [ConfigService, PortainerService],
 })
