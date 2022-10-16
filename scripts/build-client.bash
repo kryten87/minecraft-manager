5af315b49c5381e73863faca5549ddd60b7a56a4
Dave Thomas <davet@ifit.com>
2022-10-16 15:53:41 -0700

    chore: update client build script

--- scripts/build-client.bash
+++ scripts/build-client.bash
@@ -2,18 +2,24 @@
 
 IMAGE_NAME=registry.gitlab.com/minecraft-manager/client
 
-# get the version from package.json
-VERSION=$(node -e "console.log(JSON.parse(require('fs').readFileSync('package.json').toString()).version);")
+cd packages/client
 
+# get the version from package.json
+VERSION=$(node -e "console.log(JSON.parse(require('fs').readFileSync('./package.json').toString()).version);")
+
+echo -e "\nBuilding client bundle version $VERSION"
 # build the application
 rm -rf build
 yarn build
 
+
 # build the docker image
 echo -e "\nBuilding Docker image version $VERSION"
 
+cd -
+
 docker build \
-  -f ./docker/Dockerfile \
+  -f ./docker/Dockerfile.client \
   -t "$IMAGE_NAME:latest" \
   .
 
