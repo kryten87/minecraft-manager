9225ebba631b55cd817ddc13e91751e45064d918
Dave Thomas <davet@ifit.com>
2022-10-17 08:26:49 -0700

    chore: correct logging in bootstrap function

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { LogLevel } from '@nestjs/common';
 
 async function bootstrap() {
   let logger = `${process.env.LOG_LEVELS || ''}`.split(',').filter(Boolean);
@@ -8,10 +9,12 @@ async function bootstrap() {
     logger = ['error', 'warn'];
   }
   const app = await NestFactory.create(AppModule, {
-    logger,
+    logger: logger as LogLevel[],
   });
   const config = app.get<ConfigService>(ConfigService);
   const port = config.get<number>('PORT') || 3000;
+
+  console.log(`[bootstrap] Application listening on port ${port}`);
   await app.listen(port);
 }
 bootstrap();
