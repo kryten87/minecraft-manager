2be6d992b783c24a72daa8f5dd9920ff8c469962
Dave Thomas <davet@ifit.com>
2022-10-17 08:18:35 -0700

    chore: initialize log levels from env

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let logger = `${process.env.LOG_LEVELS || ''}`.split(',').filter(Boolean);
+  if (logger.length === 0) {
+    logger = ['error', 'warn'];
+  }
+  const app = await NestFactory.create(AppModule, {
+    logger,
+  });
   const config = app.get<ConfigService>(ConfigService);
   const port = config.get<number>('PORT') || 3000;
   await app.listen(port);
